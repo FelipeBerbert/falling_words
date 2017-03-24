0452e25f99d40d187118ad25bfc67df577a3c4d8
FelipeBerbert <berberthd@gmail.com>
2017-03-23 22:05:32 -0300

    add model class for "words"

---
+++ app/src/main/java/br/felipeberbert/fallingwords/model/Word.java
@@ -0,0 +1,19 @@
+package br.felipeberbert.fallingwords.model;
+
+/**
+ * Created by Felipe Berbert on 23/03/2017.
+ */
+
+public class Word { // I guess that technically they are not "words" but let's abstract this fact for this simple app
+
+    private String textEng;
+    private String textSpa;
+
+    public String getTextEng() {
+        return textEng;
+    }
+
+    public String getTextSpa() {
+        return textSpa;
+    }
+}
